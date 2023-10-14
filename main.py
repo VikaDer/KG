eaecb819a3470d82822681b47e38d8ff8898a393
vvderkach1 <vvderkach@edu.hse.ru>
2023-10-14 23:08:11 +0300

    Adding files. Imread, imshow funstion

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 import cv2
-print(cv2.__version__)
+import glob;
+
+path = glob.glob('assets/*.jpg')
+cv_img = []
+for img in path:
+    n = cv2.imread(img)
+    cv_img.append(n)
+    cv2.imshow('Image', n)
+    cv2.waitKey(0)
